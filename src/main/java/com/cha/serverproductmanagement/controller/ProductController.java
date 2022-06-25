7a3b09e95ea26ad3b87534dc8b1cc5197a751f53
Chamo96 <55450539+Chamo96@users.noreply.github.com>
2022-06-25 08:27:41 +0530

    Updated searching with filtering

--- src/main/java/com/cha/serverproductmanagement/controller/ProductController.java
+++ src/main/java/com/cha/serverproductmanagement/controller/ProductController.java
@@ -4,9 +4,13 @@ import com.cha.serverproductmanagement.model.ApiResponse;
 import com.cha.serverproductmanagement.model.Product;
 import com.cha.serverproductmanagement.model.ProductPage;
 import com.cha.serverproductmanagement.model.StringResponse;
+import com.cha.serverproductmanagement.repository.ProductRepository;
 import com.cha.serverproductmanagement.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +19,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
     @Autowired
     private ProductService productService;
 
@@ -58,5 +66,15 @@ public class ProductController {
         response.setResponse(number.toString());
 
         return new ResponseEntity<>(number, HttpStatus.OK);
+    }
+
+    //Get search with pagination (Repo and controller)
+    @GetMapping("/api/discover/new/products")
+    public Page<Product> getProductsWitSearch(@RequestParam Optional<String> query,
+                                              @RequestParam Optional<Integer> pageNumber, ProductPage productPage) {
+
+        Sort sort = Sort.by(productPage.getSortDirection(), productPage.getSortBy());
+        Pageable pageable = PageRequest.of(pageNumber.orElse(0), 6, sort);
+        return productRepository.searchProductsWithPagination(query.orElse("_"), pageable);
     }
 }
--- src/main/java/com/cha/serverproductmanagement/repository/ProductRepository.java
+++ src/main/java/com/cha/serverproductmanagement/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.cha.serverproductmanagement.repository;
 
 import com.cha.serverproductmanagement.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,4 +14,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.name LIKE CONCAT('%',:query,'%') OR p.explanation LIKE CONCAT('%',:query,'%')")
     List<Product> searchProducts(String query);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE CONCAT('%',:query,'%') OR p.explanation LIKE CONCAT('%',:query,'%')")
+    Page<Product> searchProductsWithPagination(String query, Pageable pageable);
 }
